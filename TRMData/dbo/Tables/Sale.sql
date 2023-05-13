2fe043c191da395739e6537c5a8f88f49f53540c
Özgür Ertürk <ozgur_6155@hotmail.com>
2023-05-13 13:56:06 +0300

    Add Additional Item Info
    Adds item quantity and price to the list view.

--- TRMData/dbo/Tables/Sale.sql
+++ TRMData/dbo/Tables/Sale.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Sale]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [CashierId] NCHAR(128) NOT NULL, 
+    [CashierId] NVARCHAR(128) NOT NULL, 
     [SaleDate] DATETIME2 NOT NULL,
     [SubTotal] MONEY NOT NULL,
     [Tax] MONEY NOT NULL,
